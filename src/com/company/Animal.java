dbf6b6e44977aecf4a02c8582782d2cfcea8dbb8
Wojtek <kwasniak23@gmail.com>
2020-05-01 19:14:01 +0200

    Add new  methods feed() and takeForAWalk

--- src/com/company/Animal.java
+++ src/com/company/Animal.java
@@ -1,4 +1,32 @@
 package com.company;
 
+import java.util.WeakHashMap;
+
 public class Animal {
+
+
+    String species;
+    private Double weight;
+
+    public Animal() {
+        this.weight = 10.0;
+    }
+
+    void feed(){
+
+        weight+=2;
+
+
+    }
+
+    void takeForAWalk(){
+
+        if(weight>0) {
+            weight -= 2;
+            System.out.println("add 2kg");
+            System.out.println("My weight="+weight);
+        }else{
+            System.out.println("Ziom, nie mam masy!!!");
+        }
+    }
 }
